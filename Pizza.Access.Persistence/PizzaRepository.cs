f531820de1b62ecfbf8c13723e47579a1530dd7a
Kampferfrost <kampferfrost@gmail.com>
2025-04-23 21:08:22 +0300

    add Create

--- Pizza.Access.Persistence/PizzaRepository.cs
+++ Pizza.Access.Persistence/PizzaRepository.cs
@@ -9,11 +9,29 @@ namespace NendoPizza.Access.Persistence
     public class PizzaRepository : IPizzaRepository
     {
         private PizzaDbContext pizzaDbContext = new PizzaDbContext();
+
+        public int Create(Pizza pizza)
+        {
+            using var context = pizzaDbContext;
+            context.Pizzas.Add(pizza);
+            var count = context.SaveChanges();
+            return 1;
+        }
+
+        public Pizza GetPizza(int id)
+        {
+            throw new NotImplementedException();
+        }
+
         public List<Pizza> GetPizzas()
         {
             using var context = pizzaDbContext;
             return context.Pizzas.ToList();
         }
 
+        public int Update(Pizza pizza)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Pizza.Domain/Interfaces/IPizzaRepository.cs
+++ Pizza.Domain/Interfaces/IPizzaRepository.cs
@@ -2,10 +2,15 @@
 
 namespace NendoPizza.Domain.Interfaces
 {
-    // дописать контракты
     public interface IPizzaRepository
     {
         public List<Pizza> GetPizzas();
-        
+
+        public Pizza GetPizza(int id);
+
+        public int Create(Pizza pizza);
+
+        public int Update(Pizza pizza);
     }
+
 }
