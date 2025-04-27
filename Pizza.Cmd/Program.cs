b8ed3ce76614787d0573fbde56901b12faf34aa6
Kampferfrost <kampferfrost@gmail.com>
2025-04-27 18:25:43 +0300

    NendoPizza.Cmd Add method GetPizzaById

--- Pizza.Cmd/Program.cs
+++ Pizza.Cmd/Program.cs
@@ -14,26 +14,44 @@ namespace NendoPizza.Cmd
             services.AddScoped<PizzaDbContext>();
             services.AddScoped<IPizzaRepository, PizzaRepository>();
             var serviceProvider = services.BuildServiceProvider();
-
             var pizzaRepository = serviceProvider?.GetService<IPizzaRepository>();
-            GetPizzas(pizzaRepository);
-
-            pizzaRepository.Create(new Pizza() {Name = "Trololo lie trodo" });
 
             GetPizzas(pizzaRepository);
+            AddPizza(pizzaRepository);
+            GetPizzaById(pizzaRepository);
+            GetPizzas(pizzaRepository);
+        }
+
+        private static void GetPizzaById(IPizzaRepository? pizzaRepository)
+        {
+            Console.WriteLine();
+            Console.WriteLine("***Поиск пиццы по ИД***");
+            Console.WriteLine();
+            var foundPizza = pizzaRepository?.GetPizza(2);
+            Console.WriteLine(foundPizza.Name);
+            Console.WriteLine("Найденая пицца по ID:");
+            Console.WriteLine();
+            Console.WriteLine("___Конце поиска пиццы по ИД___");
+        }
+
+        private static void AddPizza(IPizzaRepository? pizzaRepository)
+        {
+            pizzaRepository.Create(new Pizza() { Name = "Trololo lie trodo" });
         }
 
         private static void GetPizzas(IPizzaRepository? pizzaRepository)
         {
-            Console.WriteLine("_______________________________________________");
+            Console.WriteLine();
+            Console.WriteLine("***Вывод всех пицц*****************************");
+            Console.WriteLine();
             var listNendoPizzas = pizzaRepository?.GetPizzas();
             foreach (var pizza in listNendoPizzas)
             {
                 Console.WriteLine(pizza.Name);
-                Console.WriteLine("***");
+                Console.WriteLine("###");
             }
             Console.WriteLine();
-            Console.WriteLine("_______________________________________________");
+            Console.WriteLine("___Конец вывода________________________________");
             Console.WriteLine();
         }
     }
